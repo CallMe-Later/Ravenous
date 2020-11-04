7028fafb60702771e7ff8e26149061f045c17a78
Tanya <v.tianav2@gmail.com>
2020-11-04 23:34:55 +0300

    add onChange attributes to each input in render method

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -10,7 +10,11 @@ class SearchBar extends React.Component {
       term: '',
       location: '',
       sortBy: 'best_match'
+
     };
+
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
 
     this.sortByOptions = {
       "Best Match": 'best_match',
@@ -52,8 +56,8 @@ class SearchBar extends React.Component {
       </ul>
     </div>
     <div className="SearchBar-fields">
-      <input placeholder="Search Businesses" />
-      <input placeholder="Where?" />
+      <input placeholder="Search Businesses" onChange={this.handleTermChange} />
+      <input placeholder="Where?" onChange={this.handleLocationChange}/>
     </div>
     <div className="SearchBar-submit">
       <a>Let's Go</a>
