6d9f436183119c3ef224c6ff429c60bf1eff555b
Tanya <v.tianav2@gmail.com>
2020-11-06 01:26:14 +0300

    remove business and array business

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,19 +5,9 @@ import Yelp from '../../unit/Yelp.js';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
-const business = {
-  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
 
-const businessArray = [business, business, business, business, business, business];
+
+
 
 class App extends React.Component {
   searchYelp(term, location, sortBy) {
